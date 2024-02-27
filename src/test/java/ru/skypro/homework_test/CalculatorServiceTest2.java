148360ea1fae856f5fcab89dafd885f10abed22f
WalterMitty23 <23alexold23@gmail.com>
2024-02-27 08:36:28 -0500

    Done

---
+++ src/test/java/ru/skypro/homework_test/CalculatorServiceTest2.java
@@ -0,0 +1,50 @@
+package ru.skypro.homework_test;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorServiceTest2 {
+    CalculatorService calculatorService = new CalculatorService();
+    public static Stream<Arguments> args() {
+        return Stream.of(
+                Arguments.of(1, 2),
+                Arguments.of(-1, 2),
+                Arguments.of(1, -2),
+                Arguments.of(-1, -2),
+                Arguments.of(0, 2));
+    }
+
+    @ParameterizedTest
+    @MethodSource("args")
+    void namePlus(Integer num1, Integer num2) {
+        assertEquals(num1 + num2, calculatorService.plus(num1, num2));
+    }
+
+    @ParameterizedTest
+    @MethodSource("args")
+    void nameMinus(Integer num1, Integer num2) {
+        assertEquals(num1 - num2, calculatorService.minus(num1, num2));
+    }
+
+    @ParameterizedTest
+    @MethodSource("args")
+    void nameMultiply(Integer num1, Integer num2) {
+        assertEquals(num1 * num2, calculatorService.multiply(num1, num2));
+    }
+
+    @ParameterizedTest
+    @MethodSource("args")
+    void nameDivide(Integer num1, Integer num2) {
+        if (num2 == 0) {
+            assertThrows(IllegalArgumentException.class, () -> calculatorService.divide(num1, num2));
+            return;
+        }
+        assertEquals(num1 / num2, calculatorService.divide(num1, num2));
+    }
+
+}
