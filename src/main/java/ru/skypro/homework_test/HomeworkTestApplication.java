7ec94ef1083d84da3421eaf0dee3698077ec956f
WalterMitty23 <23alexold23@gmail.com>
2024-02-27 05:58:43 -0500

    Started

---
+++ src/main/java/ru/skypro/homework_test/HomeworkTestApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.homework_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkTestApplication.class, args);
+	}
+
+}
